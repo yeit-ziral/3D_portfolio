6bd29a02ae4cb8e09e4fb12408254f9dc636092c
yeit-ziral <qwg1447@gmail.com>
2023-11-12 12:44:07 +0900

    fghjk

---
+++ DirectX3D/Project/GameScene.h
@@ -0,0 +1,21 @@
+#pragma once
+class GameScene : public Scene
+{
+public:
+	GameScene();
+	~GameScene();
+
+	virtual void Update()		override;
+	virtual void PreRender()	override;
+	virtual void Render()		override;
+	virtual void PostRender()	override;
+
+private:
+	Quad* floor;
+
+	Groot* groot;
+
+	Model* bunny;
+
+	Sphere* sphere;
+};
